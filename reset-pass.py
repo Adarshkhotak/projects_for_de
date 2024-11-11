5d3ad508e77fc0461f00d443a8aa89ed8af0cbd5
Adarsh <adarshkhot72@gmail.com>
2024-11-11 16:06:29 +0530

    added reset password

---
+++ reset-pass.py
@@ -0,0 +1,7 @@
+username = input("Entere user name:")
+password= input("Enter password:")
+
+if username =="Admin" and password =="Admin":
+    print("u can reset")
+else:
+    print("u cannot reset")
